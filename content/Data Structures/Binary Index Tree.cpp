73fbfb4e0b4a929adb6989545ec4e962d772e607
vanloc1808 <vanloc1808@gmail.com>
2022-01-28 19:33:26 +0700

    binary index tree

---
+++ content/Data Structures/Binary Index Tree.cpp
@@ -0,0 +1,25 @@
+struct BIT {
+     int n;
+     int t[2 * N];
+
+     void add(int where, long long what) {
+        for (where++; where <= n; where += where & -where) {
+            t[where] += what;
+        }
+     }
+
+     void add(int from, int to, long long what) {
+         add(from, what);
+         add(to + 1, -what);
+     }
+
+     long long query(int where) {
+         long long sum = t[0];
+
+         for (where++; where > 0; where -= where & -where) {
+             sum += t[where];
+         }
+
+         return sum;
+     }
+};
